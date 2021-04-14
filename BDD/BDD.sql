094584939a90c55b6d912a91c07f8d1dc5f464d6
LeFanDOst <lefandost@gmail.com>
2021-04-14 15:13:56 +0200

    Ajout d'un admin dans les données de test

--- BDD/BDD.sql
+++ BDD/BDD.sql
@@ -88,27 +88,28 @@ CREATE TABLE IF NOT EXISTS Preinscription
 	CONSTRAINT FK_Preinscription_Tournoi FOREIGN KEY (idTournoi) REFERENCES Tournoi(idTournoi) ON UPDATE CASCADE ON DELETE CASCADE
 ) DEFAULT CHARSET=utf8;
 
-INSERT INTO Utilisateur VALUES(0, "DUJARDIN", "Jean", "JeanDujardin@test.com", "4ff17bc8ee5f240c792b8a41bfa2c58af726d83b925cf696af0c811627714c85", "Utilisateur"); -- Mot de passe : jean
+INSERT INTO Utilisateur VALUES(0, "ADMIN", "Admin", "admin@test.com", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", "Administrateur"), -- Mot de passe : admin
+(1, "DUJARDIN", "Jean", "JeanDujardin@test.com", "4ff17bc8ee5f240c792b8a41bfa2c58af726d83b925cf696af0c811627714c85", "Utilisateur"); -- Mot de passe : jean
 
-INSERT INTO Gestionnaire VALUES(0);
+INSERT INTO Gestionnaire VALUES(1);
 
 INSERT INTO Tournoi VALUES(0, "Tournoi Tampon", "2021-04-08", "30", 0, "Montpellier");
 INSERT INTO Tournoi VALUES(1, "Championnat du vin", "2021-04-08", "30", 0, "Montpellier");
 INSERT INTO Tournoi VALUES(2, "Tournoi des saucissons", "2021-04-08", "30", 0, "Montpellier");
 INSERT INTO Tournoi VALUES(3, "Compétition de la vodka", "02021-04-08", "30", 0, "Montpellier");
 
-INSERT INTO Utilisateur VALUES(1, "Machin", "Truc", "M@T.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(2, "Jean", "Dupont", "J@D.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(3, "Henri", "Guibet", "H@G.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(4, "Louis", "De Funès", "L@F.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(5, "Jean", "Gabin", "J@G.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(6, "Robert", "Redford", "R@R.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(7, "Lino", "Ventura", "L@V.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(8, "Francis", "Blanche", "F@B.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(9, "Venantino", "Venantini", "V@V.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(10, "Jean", "Lefevre", "J@L.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(11, "Bernard", "Blier", "B@B.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
-(12, "Line", "Renaud", "M@T.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur");
+INSERT INTO Utilisateur VALUES(2, "Machin", "Truc", "M@T.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(3, "Jean", "Dupont", "J@D.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(4, "Henri", "Guibet", "H@G.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(5, "Louis", "De Funès", "L@F.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(6, "Jean", "Gabin", "J@G.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(7, "Robert", "Redford", "R@R.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(8, "Lino", "Ventura", "L@V.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(9, "Francis", "Blanche", "F@B.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(10, "Venantino", "Venantini", "V@V.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(11, "Jean", "Lefevre", "J@L.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(12, "Bernard", "Blier", "B@B.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur"),
+(13, "Line", "Renaud", "M@T.com", "74913f96f46a13995ef292f85deffae7b86a35d5d3180a5581b04b12b7b30245", "Utilisateur");
 -- Mot de passes des comptes 1 à 12 : unMotDePasse
 
 INSERT INTO Equipe VALUES(0, "Équipe 1", 0, "Une adresse 1", "04-06-04-06-04"),
@@ -117,15 +118,15 @@ INSERT INTO Equipe VALUES(0, "Équipe 1", 0, "Une adresse 1", "04-06-04-06-04"),
 (3, "Équipe 4", 0, "Une adresse 4", "04-06-04-06-04"),
 (4, "Équipe 5", 0, "Une adresse 5", "04-06-04-06-04");
 
-INSERT INTO Joueur VALUES(1, 0, true),
-(2, 0, false),
+INSERT INTO Joueur VALUES(2, 0, true),
 (3, 0, false),
-(4, 1, true),
-(5, 1, false),
+(4, 0, false),
+(5, 1, true),
 (6, 1, false),
-(7, 3, true),
-(8, 3, false),
+(7, 1, false),
+(8, 3, true),
 (9, 3, false),
-(10, 4, true),
-(11, 4, false),
-(12, 4, false);
+(10, 3, false),
+(11, 4, true),
+(12, 4, false),
+(13, 4, false);
